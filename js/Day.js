eb691e4b5250e7cf14740fad830d9623ea032387
Rianna Suen <vividfax@gmail.com>
2023-11-11 15:33:24 +0000

    Init

---
+++ js/Day.js
@@ -0,0 +1,31 @@
+class Day {
+
+    constructor () {
+
+        this.loop = true;
+    }
+
+    prerun() {
+
+    }
+
+    update() {
+
+    }
+
+    mousePressed() {
+
+    }
+
+    mouseReleased() {
+
+    }
+
+    keyPressed() {
+
+    }
+
+    keyReleased() {
+
+    }
+}
